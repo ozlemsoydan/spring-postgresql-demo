d35646e2e24201b2287cb9d66da3faef1cba80ac
ozlemsoydan <71570727+ozlemsoydan@users.noreply.github.com>
2023-03-14 23:38:16 +0300

    initializr

---
+++ src/main/java/com/example/springpostgresqldemo/SpringPostgresqlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpostgresqldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPostgresqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPostgresqlDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springpostgresqldemo/SpringPostgresqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpostgresqldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPostgresqlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
